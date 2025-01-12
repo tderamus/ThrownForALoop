99b58db7376b166f1ee54c71952600932052f998
tderamus <deramust@gmail.com>
2025-01-11 21:53:19 -0600

    added while loop to handle user input selection presented by list menu options

--- Program.cs
+++ Program.cs
@@ -30,4 +30,12 @@ for (int i = 0; i < products.Count; i++)
     Console.WriteLine($"{i + 1}. {products[i]}");
 }
 
-int response = int.Parse(Console.ReadLine().Trim());
+int response = int.Parse(Console.ReadLine());
+
+// Verify the user entered a value that evaluates to true or false
+while (response > products.Count || response < 1)
+{
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
+}
+Console.WriteLine($"You chose: {products[response -1]}");
