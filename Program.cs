29354b09825feb5f7d82ef1fa27458e460044416
tderamus <deramust@gmail.com>
2025-01-11 20:54:29 -0600

    added logic for string input handling as well as integer input handling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Threading.Channels;
+
+string greeting = @"Welcom to Thrown For a Loop
+your one-stop shop for used sporting equipment";
+Console.WriteLine(greeting);
+
+
+Console.WriteLine(@"Products:
+    1. Football
+    2. Hockey stick
+    3. Boomeranng
+    4. Frisbee
+    5. Golf Putter");
+Console.WriteLine("Please enter a product number:");
+
+int response = int.Parse(Console.ReadLine().Trim());
+
+
+// Verify the user entered a value that evaluates to true or false
+while ( response > 5 || response < 1)
+{
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
+}
+Console.WriteLine($"You chose: {response}");
+
