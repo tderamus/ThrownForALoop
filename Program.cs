fb1689a6beed706e10d9a84076f48353fe3e608a
tderamus <deramust@gmail.com>
2025-01-11 22:40:05 -0600

    added Product class in Product.cs file, created new intances of Product in Programs.cs file to replace the string product list for user selections

--- Program.cs
+++ Program.cs
@@ -1,41 +1,30 @@
-﻿List<int> years = new List<int> 
-{ 
-    2010, 
-    2011, 
-    2012, 
-    2013, 
-    2014, 
-    2015, 
-    2016, 
-    2017, 
-    2018, 
-    2019, 
-    2020 
+﻿
+List<Product> products = new List<Product>()
+{
+    new Product() { Name = "Football", Price = 25, Sold = false },
+    new Product() { Name = "Hockey Stick", Price = 50, Sold = false },
+    new Product() { Name = "Boomerang", Price = 10, Sold = false },
+    new Product() { Name = "Frisbee", Price = 15, Sold = false },
+    new Product() { Name = "Golf Putter", Price = 75, Sold = false }
 };
 
-List<String> products = new List<String>()
-{
-    "Football",
-    "Hockey Stick",
-    "Boomerang",
-    "Fresbee",
-    "Golf Putter"
-};
 
 // Display menu options from our list of products
 Console.WriteLine("Products");
 Console.WriteLine("Please enter a product number: ");
 for (int i = 0; i < products.Count; i++)
 {
-    Console.WriteLine($"{i + 1}. {products[i]}");
+    Console.WriteLine($"{i + 1}. {products[i].Name}");
 }
-
+Console.WriteLine();
 int response = int.Parse(Console.ReadLine());
 
-// Verify the user entered a value that evaluates to true or false
 while (response > products.Count || response < 1)
 {
     Console.WriteLine("Choose a number between 1 and 5!");
     response = int.Parse(Console.ReadLine().Trim());
 }
-Console.WriteLine($"You chose: {products[response -1]}");
+
+Product chosenProduct = products[response - 1];
+Console.WriteLine(@$"You chose: {chosenProduct.Name}, which cost ${chosenProduct.Price}
+dollars and is {(chosenProduct.Sold ? "" : "not ")}sold ");
